d796d59749eddd144abd3cf973c65e8870e50cad
rsuntk <rsuntk@yukiprjkt.my.id>
2025-07-22 10:39:58 +0700

    build: Silent fetching
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ fi
 
 if ! [ -d "$HOME/zyc-clang" ]; then
 echo "- Toolchains not found! Fetching..."
-aria2c https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/0998f421320ae02fddabec8a78b91bf7620159f6/clang-r563880.tar.gz
+aria2c https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/20052113d2a5552cfff78ac7b21fd7953cc1c592/clang-r563880.tar.gz 2>/dev/null
 mkdir ~/clang
 tar -xf *.tar.gz -C ~/clang
 [ ! -d "$HOME/androidcc-4.9" ] && curl -LSs "https://raw.githubusercontent.com/rsuntk/toolchains/refs/heads/README/clone.sh" | bash -s androidcc-4.9
